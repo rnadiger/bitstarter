89f3eff525d663b5aed0d8f74a50fad261f58b6c
Ravi Nadiger <r_nadiger@yahoo.com>
2013-07-16 01:57:39 +0000

    update web.js 5

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var output = fs.readFileSync('index.html','utf8');
   response.send(output);
-//  response.send('Hello World 2!');
+////  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
